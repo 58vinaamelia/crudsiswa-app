03b7f643bdd829e57641cf4356db6f828c25d722
58vinaamelia <vinameow011225imut@gmail.com>
2025-07-31 07:19:28 +0700

    revisi model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
     }
     public function clas()
     {
-        return $this->belongsTo(Clas::class, 'clas_id');
+        return $this->belongsTo(Clas::clas, 'clas_id');
     }
 
 }
